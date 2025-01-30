094650791b38cf8166ba4b5006442ec5c29aa5fc
GabrielBarbosa0 <82109149+GabrielBarbosa0@users.noreply.github.com>
2025-01-30 15:13:47 -0300

    organização de downloads

--- Python 3 – Mundo 1/organizar_downloads.py
+++ Python 3 – Mundo 1/organizar_downloads.py
@@ -7,12 +7,14 @@ downloads_folder = os.path.expanduser("~/Downloads")
 # Categorias e extensões correspondentes
 categories = {
     "Compressed": ["7z", "7zip", "rar", "zip", "tar", "gz", "bz2", "xz", "lzma", "z", "lz", "iso", "war", "apk"],
-    "Imagens": ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "heif", "heic", "svg", "ico", "avif"],
+    "Imagens": ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "heif", "heic", "svg", "ico", "avif", "jfif"],
     "Vídeos": ["mp4", "avi", "mkv", "mov", "wmv", "flv", "webm", "mpg", "mpeg", "3gp", "m4v", "ogv"],
     "Músicas": ["mp3", "wav", "flac", "aac", "ogg", "wma", "m4a", "alac", "opus", "amr"],
     "Documentos": ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "rtf", "odt", "ods", "odp", "csv"],
     "Scripts": ["py", "js", "html", "css", "java", "c", "cpp", "cs", "php", "rb", "go", "ts"],
-    "Design e Modelagem 3D": ["psd", "ai", "eps", "cdr", "sketch", "fig", "fbx", "obj", "stl", "glb"],
+    "Design e Modelagem 3D": ["psd", "ai", "eps", "cdr", "sketch", "fig", "fbx", "obj", "stl", "glb" "ply", "gltf", "blend", "max", "c4d", "ma", "mb"],
+    "Arquivos SketchUp": ["skp", "rpz"],
+    "Projetos Revit": ["rvt", "rte", "rfa"],
     "Programas": ["exe", "msi", "dmg", "sh", "deb", "rpm", "bat", "com", "jar"],
     "Arquivos AutoCAD": ["dwg", "dxf", "dwt", "ctb", "stb", "plt", "shx", "pat", "lin", "pc3", "arg", "bak", "sv$", "ac$"]
 }
@@ -45,3 +47,5 @@ def organize_downloads(folder):
 if __name__ == "__main__":
     organize_downloads(downloads_folder)
     print("Organização concluída!")
+
+# Desenvolvido por GabrielBarbosa0 Github!
